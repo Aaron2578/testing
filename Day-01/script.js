e6dc4c4d6cf801fbe0d004b290e49c4c8216cf68
Aaron2578 <aaronchrist2578@gmail.com>
2024-01-07 19:18:43 +0530

    Script is created & updates in html and css

---
+++ Day-01/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x,y=10;
+    var z=x+y;
+    document.getElementById('demo').innerHTML='World';
+    document.getElementById('demo').style.color='Green';
+    document.getElementById('demo').style.fontSize='2em';
+    document.getElementById('demo').style.fontWeight='bolder';
+}
